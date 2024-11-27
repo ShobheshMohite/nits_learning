aeae6ad9e6917ee0f1d5823e14cdace97b3dc132
Shobhesh Sambhaji Mohite <shobheshsmohite007@gmail.com>
2024-11-27 11:27:11 +0530

    forms

---
+++ Php_forms/Date.php
@@ -0,0 +1,27 @@
+<!-- <?php
+$d=mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+?> -->
+
+<!-- <?php
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+?> -->
+
+<!-- //calculate next 6 saturdays -->
+
+<?php
+$startdate = strtotime("Saturday");
+$enddate = strtotime("+6 weeks", $startdate);
+
+while ($startdate < $enddate) {
+  echo date("M d", $startdate) . "<br>";
+  $startdate = strtotime("+1 week", $startdate);
+}
+?>
---
+++ Php_forms/index.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<div class="menu">
+<?php include 'includeDemo.php';?>
+</div>
+
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+
+</body>
+</html>
