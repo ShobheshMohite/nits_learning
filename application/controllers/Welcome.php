5ed1987c4d333e300b285c41c41f9869ae2a1a0f
Shobhesh Sambhaji Mohite <shobheshsmohite007@gmail.com>
2024-12-02 11:15:29 +0530

    codeigniterDemo

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('about');
 	}
 	public function signup(){
 		echo 'Demo..';
